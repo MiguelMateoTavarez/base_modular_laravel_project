c1cf5d9319b0d23064f7dbfff9ccc79206c7f47f
Miguel Mateo <miguel.mateo@bitboxonline.com>
2024-09-12 14:48:53 -0400

    feat: create login and register requests

---
+++ modules/Authentication/Http/Requests/LoginRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Authentication\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string'],
+            'device_name' => ['required', 'string'],
+        ];
+    }
+}
---
+++ modules/Authentication/Http/Requests/RegisterRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Authentication\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'],
+            'device_name' => ['required', 'string'],
+        ];
+    }
+}
