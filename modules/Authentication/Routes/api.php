e8920c9ae00e26c6718767a9e86e00084cfaca57
Miguel Mateo <miguel.mateo@bitboxonline.com>
2024-09-12 14:57:21 -0400

    feat: create login, register and logout routes

--- modules/Authentication/Routes/api.php
+++ modules/Authentication/Routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Authentication\Http\Controllers\LogoutController;
+use Modules\Authentication\Http\Controllers\RegisterController;
 
-Route::prefix('v1/Authentication')->group(function () {
-    //
+Route::prefix('v1/auth')->group(function () {
+    Route::post('register', RegisterController::class);
+    Route::post('login', RegisterController::class);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::post('logout', LogoutController::class);
+    });
 });
