131aef2ef96d823f94e84ce595b8778cac4e4a14
Miguel Mateo <miguel.mateo@bitboxonline.com>
2024-10-11 14:21:36 -0400

    refactor: create service command

--- app/Console/Commands/CreateServiceForModule.php
+++ app/Console/Commands/CreateServiceForModule.php
@@ -2,77 +2,71 @@
 
 namespace App\Console\Commands;
 
+use App\Console\shared\CommandFactory;
 use App\Console\shared\CustomPathTrait;
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Str;
 
-class CreateServiceForModule extends Command
+class CreateServiceForModule extends CommandFactory
 {
-    use CustomPathTrait;
-
     protected $signature = 'make:module-service {module} {service} {--i|interface= : The interface to implement} {--p|path= : Custom path}';
     protected $description = 'Create a service for a module';
-    protected Filesystem $files;
 
-    public function __construct(Filesystem $files)
-    {
-        parent::__construct();
-        $this->files = $files;
-    }
+    protected string $directoryPath = 'Eloquents/Services';
+    private string $interfacePath = 'Eloquents/Contracts';
+    protected string $stubPath = '/Console/Stubs/service_contract.stub';
+    protected string $alternativeStubPath = '/Console/Stubs/service_no_contract.stub';
 
     /**
      * @throws FileNotFoundException
      */
     public function handle(): void
     {
-        $moduleName = Str::title($this->argument('module'));
-        $customPath = $this->getCustomPath();
-        $basePath = is_null($customPath)
-            ? base_path('modules' . DIRECTORY_SEPARATOR . $moduleName . DIRECTORY_SEPARATOR . 'Eloquents/Services')
-            : base_path('modules' . DIRECTORY_SEPARATOR . $moduleName . DIRECTORY_SEPARATOR . 'Eloquents/Services/' . $customPath);
-        $seederName = $this->getTitleFormatted();
-        $interfaceName = $this->option('interface');
-
-        $seederPath = "{$basePath}/{$seederName}.php";
-
-        if($this->files->exists($seederPath)){
-            $this->warn('The file already exists');
-            return;
-        }
-
-        if(!$this->files->exists(base_path("modules/{$moduleName}/Eloquents/Contracts/{$interfaceName}.php"))){
-            $this->error("The interface '{$interfaceName}' doesn't exists");
-            return;
-        }
-
-        if(!$this->files->isDirectory($basePath)){
-            $this->files->makeDirectory($basePath,0755, true);
-        }
-
-        $modelStub = $interfaceName
-            ? app_path("/Console/Stubs/service_contract.stub")
-            : app_path("/Console/Stubs/service_no_contract.stub");
-
-        $stubContent = $this->files->get($modelStub);
-
-        $stubContent = str_replace('{{ serviceName }}', $seederName, $stubContent);
-        $stubContent = str_replace('{{ moduleName }}', $moduleName, $stubContent);
-        $stubContent = str_replace('{{ namespace }}', $this->getNameSpace(), $stubContent);
+        $moduleName = $this->getArgumentCapitalized($this->argument('module'));
+        $basePath = $this->getBasePath($this->getCustomPath(), $moduleName);
+        $interfaceBasePath = $this->getBasePath($this->getInterfacePath(), $moduleName);
+        $serviceName = $this->getArgumentCapitalized($this->argument('model'));
+        $interfaceName = $this->getArgumentCapitalized($this->option('interface'));
 
         if($interfaceName){
-            $stubContent = str_replace('{{ interfaceName }}', $interfaceName, $stubContent);
+            $this->updatePlaceHolders($interfaceName);
         }
 
-        $this->files->put($seederPath, $stubContent);
+        $this->setPlaceHolders($serviceName, $moduleName);
 
-        $this->info("{$seederName} created successfully for the module {$moduleName}");
+        $servicePath = $this->getResourcePath($basePath, $serviceName);
+        $interfacePath = $this->getResourcePath($interfaceBasePath, $interfaceName);
+
+        $this->verifyIfResourceExists($servicePath, "Service '{$interfacePath}' already exists.");
+        $this->verifyIfInterfaceExists($interfacePath, "The interface '{$interfaceName}' doesn't exists.");
+
+        $this->createDirectoryForResource($basePath);
+
+        $this->createResource($interfacePath, $serviceName, $moduleName);
+
+        $this->info("Service {$serviceName} created successfully for the module {$moduleName}");
     }
 
-    private function getTitleFormatted()
+    private function verifyIfInterfaceExists(string $resourceName, string $message): void
     {
-        $title = $this->argument('service');
-        return Str::contains($title, 'Service') ? $title : $title.'Service';
+        if (!$this->files->exists($resourceName)) {
+            throw new \RuntimeException($message);
+        }
+    }
+
+    private function getInterfacePath(): string
+    {
+        if (is_null($this->option('path'))) {
+            return $this->interfacePath;
+        }
+
+        return $this->interfacePath.'/'.$this->getClearCustomCapitalizedPath();
+    }
+
+    protected function updatePlaceHolders($interfaceName): void
+    {
+        $this->placeHolders['{{ interfaceName }}'] = $interfaceName;
     }
 }
