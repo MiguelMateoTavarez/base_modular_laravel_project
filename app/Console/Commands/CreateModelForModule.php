abd58f56b642ad69a3604a1d1387c1530de1420a
Miguel Mateo <miguel.mateo@bitboxonline.com>
2024-10-14 12:45:47 -0400

    feat: implement creation of migration with all option from model

--- app/Console/Commands/CreateModelForModule.php
+++ app/Console/Commands/CreateModelForModule.php
@@ -3,11 +3,15 @@
 namespace App\Console\Commands;
 
 use App\Console\shared\CommandFactory;
+use App\Helper\Pluralize;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Support\Facades\Artisan;
 
 class CreateModelForModule extends CommandFactory
 {
-    protected $signature = 'make:module-model {module} {model} {--p|path= : Custom path}';
+    protected $signature = 'make:module-model {module} {model}
+        {--a|all : Create all resources}
+        {--p|path= : Custom path}';
 
     protected $description = 'Create a model for a module';
 
@@ -34,6 +38,60 @@ class CreateModelForModule extends CommandFactory
         $this->createDirectoryForResource($basePath);
         $this->createResource($modelPath);
 
+        if(array_key_exists('all', $this->options())) {
+            $this->createAllResources();
+        }
+
         $this->info("Model {$modelName} created successfully for the module {$this->moduleName}");
+    }
+
+    protected function createAllResources(): void
+    {
+        $this->createMigrationByCommand();
+    }
+
+    protected function createMigrationByCommand(): void
+    {
+        $tableName = Pluralize::getPlural(strtolower($this->argument('model')));
+
+        Artisan::call('make:module-migration', [
+            'module' => $this->moduleName,
+            'migration' => "create_{$tableName}_table",
+        ]);
+    }
+
+    public function createSeederByCommand(): void
+    {
+        //todo: seeder
+    }
+
+    public function createPolicyByCommand(): void
+    {
+        //todo: policy
+    }
+
+    public function createControllerByCommand(): void
+    {
+        //todo: controller
+    }
+
+    public function createRequestByCommand(): void
+    {
+        //todo: request
+    }
+
+    public function createResourceByCommand(): void
+    {
+        //todo: resource
+    }
+
+    public function createInterfaceByCommand(): void
+    {
+        //todo: interface
+    }
+
+    public function createServiceByCommand(): void
+    {
+        //todo: service
     }
 }
--- modules/Testmodule/Enums/Second/SecondEnum.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Modules\Testmodule\Enums\Second;
-
-enum SecondEnum: string
-{
-    //
-}
