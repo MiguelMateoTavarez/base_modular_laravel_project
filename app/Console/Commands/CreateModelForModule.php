ca42cfe4529e594d81b89a2d1ac78065cd3e36f4
Miguel Mateo <miguel.mateo@bitboxonline.com>
2024-10-14 14:19:46 -0400

    feat: add all option to model creation

--- app/Console/Commands/CreateModelForModule.php
+++ app/Console/Commands/CreateModelForModule.php
@@ -19,6 +19,8 @@ class CreateModelForModule extends CommandFactory
 
     protected string $stubPath = '/Console/Stubs/model.stub';
 
+    private string $modelName;
+
     /**
      * @throws FileNotFoundException
      */
@@ -27,27 +29,32 @@ class CreateModelForModule extends CommandFactory
         parent::handle();
 
         $basePath = $this->getBasePath($this->getCustomPath());
-        $modelName = $this->capitalize($this->argument('model'));
+        $this->modelName = $this->capitalize($this->argument('model'));
 
-        $this->setPlaceHolders($modelName);
+        $this->setPlaceHolders($this->modelName);
 
-        $modelPath = $this->getResourcePath($basePath, $modelName);
+        $modelPath = $this->getResourcePath($basePath, $this->modelName);
 
         $this->verifyIfResourceExists($modelPath, 'Model already exists.');
 
         $this->createDirectoryForResource($basePath);
         $this->createResource($modelPath);
 
-        if(array_key_exists('all', $this->options())) {
+        if (array_key_exists('all', $this->options())) {
             $this->createAllResources();
         }
 
-        $this->info("Model {$modelName} created successfully for the module {$this->moduleName}");
+        $this->info("Model {$this->modelName} created successfully for the module {$this->moduleName}");
     }
 
     protected function createAllResources(): void
     {
         $this->createMigrationByCommand();
+        $this->createSeederByCommand();
+        $this->createPolicyByCommand();
+        $this->createControllerByCommand();
+        $this->createRequestByCommand();
+        $this->createResourceByCommand();
     }
 
     protected function createMigrationByCommand(): void
@@ -62,36 +69,82 @@ class CreateModelForModule extends CommandFactory
 
     public function createSeederByCommand(): void
     {
-        //todo: seeder
+        $params = [
+            'module' => $this->moduleName,
+            'seeder' => "{$this->modelName}Seeder",
+        ];
+
+        if(array_key_exists('all', $this->options())) {
+            $params['-p'] = $this->option('path');
+        }
+
+        Artisan::call('make:module-seeder', $params);
     }
 
     public function createPolicyByCommand(): void
     {
-        //todo: policy
+        $params = [
+            'module' => $this->moduleName,
+            'policy' => "{$this->modelName}Policy",
+            '-m' => $this->modelName
+        ];
+
+        if(array_key_exists('all', $this->options())) {
+            $params['-p'] = $this->option('path');
+        }
+
+        Artisan::call('make:module-policy', $params);
     }
 
     public function createControllerByCommand(): void
     {
-        //todo: controller
+        $tableName = Pluralize::getPlural(strtolower($this->argument('model')));
+
+        $params = [
+            'module' => $this->moduleName,
+            'controller' => "{$tableName}Controller",
+        ];
+
+        if(array_key_exists('all', $this->options())) {
+            $params['-p'] = $this->option('path');
+        }
+
+        Artisan::call('make:module-controller', $params);
     }
 
     public function createRequestByCommand(): void
     {
-        //todo: request
+        $storeParams = [
+            'module' => $this->moduleName,
+            'request' => "Store{$this->modelName}Request",
+        ];
+
+        $updateParams = [
+            'module' => $this->moduleName,
+            'request' => "Update{$this->modelName}Request",
+        ];
+
+        if(array_key_exists('all', $this->options())) {
+            $storeParams['-p'] = $this->option('path');
+            $updateParams['-p'] = $this->option('path');
+        }
+
+        Artisan::call('make:module-request', $storeParams);
+
+        Artisan::call('make:module-request', $updateParams);
     }
 
     public function createResourceByCommand(): void
     {
-        //todo: resource
-    }
+        $params = [
+            'module' => $this->moduleName,
+            'resource' => "{$this->modelName}Resource",
+        ];
 
-    public function createInterfaceByCommand(): void
-    {
-        //todo: interface
-    }
+        if(array_key_exists('all', $this->options())) {
+            $params['-p'] = $this->option('path');
+        }
 
-    public function createServiceByCommand(): void
-    {
-        //todo: service
+        Artisan::call('make:module-resource', $params);
     }
 }
